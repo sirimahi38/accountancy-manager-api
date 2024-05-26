8224f44ad4a723d20ff871ff7989575e74e7ff00
Mahesh.Sirikonda <Mahesh.Sirikonda@IBE-LT161090>
2024-05-26 16:15:05 +0200

    react js call integrated

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ListTasksComponent from './components/ListTasksComponent'
+
+function App() {
+  return (
+    <div>
+
+        <p>
+          <h1>Accountancy Manager</h1>
+          <ListTasksComponent/>
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ListTasksComponent.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function ListTasksComponent(){
+    const dummyData = [
+            {
+            "id":1,
+           "taskGroup":"Group1",
+           "taskName":"ITR",
+           "taskCheckList":"checkList",
+           "advanceIntimation":"",
+           "remarks":"check before approve",
+           "taskType":"M",
+           "taskAssignedTo":"Mahesh",
+           "taskStatus":"created"
+            },
+            {
+            "id":2,
+           "taskGroup":"Group1",
+           "taskName":"ITR",
+           "taskCheckList":"checkList",
+           "advanceIntimation":"",
+           "remarks":"check before approve",
+           "taskType":"M",
+           "taskAssignedTo":"Mahesh",
+           "taskStatus":"created"
+            }
+    ]
+    return (
+        <div className='container'>
+        <h2>Empoyee Tasks</h2>
+        <table className='table table-stripped table-bordered'>
+            <thead>
+                <tr>
+                    <th>Task Id</th>
+                    <th>Task Group</th>
+                    <th>Task Name </th>
+                    <th>TaskCheckList</th>
+                    <th>AdvanceIntimation</th>
+                    <th>Remarks</th>
+                    <th>TaskType</th>
+                    <th>TaskAssignedTo</th>
+                    <th>TaskStatus</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                dummyData.map(task =>
+                 <tr key={task.id}>
+                    <td>{task.id}</td>
+                    <td>{task.taskGroup}</td>
+                    <td>{task.taskName}</td>
+                    <td>{task.taskCheckList}</td>
+                    <td>{task.advanceIntimation}</td>
+                    <td>{task.remarks}</td>
+                    <td>{task.taskType}</td>
+                    <td>{task.taskAssignedTo}</td>
+                    <td>{task.taskStatus}</td>
+                </tr>)
+            }
+
+            </tbody>
+        </table>
+
+        </div>
+    )
+}
+
+export default ListTasksComponent
